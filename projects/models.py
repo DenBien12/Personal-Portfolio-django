1a24e95bbe4f8cc8be83eb743a6280bb4695e097
Dang Le Hai <haidangle678@gmail.com>
2024-07-04 23:32:34 +0700

    Final Commit

--- projects/models.py
+++ projects/models.py
@@ -6,3 +6,7 @@ class Project(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    image = models.FileField(upload_to="project_images/", blank=True)
+
+    def __str__(self):
+        return self.name
